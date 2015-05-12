25f2b4ad6e009aafbca871410e199b9a24df6690
Alan Colon <github@alanc.net>
2015-05-11 21:26:58 -0600

    CapitalizeEveryNthWordTest

--- code/tests.js
+++ code/tests.js
@@ -6,7 +6,7 @@ var tests = {
     },
     capitalizeEveryNthWordTest: function() {
         var sentence = "Lorem ipsum dolor sit amet";
-        assert.areEqual("Lorem ipsum dolor Sit amet", code.capitalizeEveryNthWord(sentence, 0, 2));
+        assert.areEqual("Lorem ipsum Dolor sit Amet", code.capitalizeEveryNthWord(sentence, 0, 2));
         assert.areEqual("Lorem ipsum Dolor Sit Amet", code.capitalizeEveryNthWord(sentence, 2, 1));
     }
 };
