7700dec431a84f723d4479268bf44da3c4de293d
gmurrayltf <gmurray@lifetimefitness.com>
2015-08-25 17:29:27 -0600

    Adjust isPrimeTest to check a wider range of numbers - 1-1000 felt good enough.  We had some code submitted that said 121 was a prime number, but it is not (11x11).

--- code/Tests.java
+++ code/Tests.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Tests {
     public static void helloWorldTest() {
         Assert.areEqual("Hello World!", Code.helloWorld());
@@ -10,11 +14,34 @@ public class Tests {
     public static void isPrimeTest() {
         Assert.isFalse(Code.isPrime(-1), "IsPrime(-1) should be false.");
         Assert.isFalse(Code.isPrime(0), "IsPrime(0) should be false.");
-        Assert.isFalse(Code.isPrime(1), "IsPrime(1) should be false.");
-        Assert.isTrue(Code.isPrime(2), "IsPrime(2) should be true.");
-        Assert.isTrue(Code.isPrime(5), "IsPrime(5) should be true.");
-        Assert.isFalse(Code.isPrime(87), "IsPrime(87) should be false.");
-        Assert.isTrue(Code.isPrime(97), "IsPrime(97) should be true.");
+
+        Set<Integer> primesTo1000 = new HashSet<Integer>(Arrays.asList(
+            2, 3, 5, 7, 11, 13, 17, 19, 23,
+            29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+            71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+            113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+            173, 179, 181, 191, 193, 197, 199, 211, 223, 227,
+            229, 233, 239, 241, 251, 257, 263, 269, 271, 277,
+            281, 283, 293, 307, 311, 313, 317, 331, 337, 347,
+            349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
+            409, 419, 421, 431, 433, 439, 443, 449, 457, 461,
+            463, 467, 479, 487, 491, 499, 503, 509, 521, 523,
+            541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
+            601, 607, 613, 617, 619, 631, 641, 643, 647, 653,
+            659, 661, 673, 677, 683, 691, 701, 709, 719, 727,
+            733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+            809, 811, 821, 823, 827, 829, 839, 853, 857, 859,
+            863, 877, 881, 883, 887, 907, 911, 919, 929, 937,
+            941, 947, 953, 967, 971, 977, 983, 991, 997));
+
+        for (int i=1; i<1000; i++) {
+            if (primesTo1000.contains(i)) {
+                Assert.isTrue(Code.isPrime(i), "IsPrime(" + i + ") should be true.");
+            }
+            else {
+                Assert.isFalse(Code.isPrime(i), "IsPrime(" + i + ") should be false.");
+            }
+        }
     }
     public static void goldenRatioTest() {
         Assert.isInRange(1.61800, 1.61806, Code.goldenRatio(1.0, 1.0));
